11fa5f03d9c7b3f3c1ae8d8d31d97cbe939fe774
Spencer Davis <spencerdavis2012@gmail.com>
2018-09-11 01:26:52 -0500

    first few assignemnts added into repo finally

---
+++ HelloPrinter/Week2/src/AverageAgeDavis.java
@@ -0,0 +1,17 @@
+
+public class AverageAgeDavis {
+	// Spencer Davis
+	public static void main(String[] args) {
+		
+		int keithAge = 30;
+		int nathanAge = 28;
+		int spencerAge = 25;
+		int bresciaAge = 23;
+		int elizaAge = 18;
+
+		double avgAgeDavisFam = ((double)keithAge + (double)nathanAge + (double)spencerAge + (double)bresciaAge + (double)elizaAge) / 5;
+		
+		System.out.println("The Davis family siblings are an average of " + avgAgeDavisFam + " years old!");
+	}
+
+}
